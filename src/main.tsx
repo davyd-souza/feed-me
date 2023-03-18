6fba8e6a5188e2213532016b5d4fc4d70db5f9ea
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-17 23:39:04 -0300

    feat: create css reset

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom/client'
 // COMPONENT
 import { App } from './App'
 
+// STYLES
+import './styles/global.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
