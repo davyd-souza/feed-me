9550d76f31620bab8b8f0f60e224cd8d71a06a2b
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-18 16:38:26 -0300

    feat: create style for app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // COMPONENT
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+
 export function App() {
   return (
     <div className='App'>
       <Header />
 
-      <div>
+      <div className={styles.container}>
         <aside>sidebar</aside>
         <main>feed</main>
       </div>
