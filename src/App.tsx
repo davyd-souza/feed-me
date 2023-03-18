b60583ff303dee4fea097dbd7bd17b3a83f19136
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-18 16:38:10 -0300

    feat: structure app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+// COMPONENT
+import { Header } from './components/Header'
+
 export function App() {
   return (
     <div className='App'>
-      <h1>Feed Me</h1>
+      <Header />
+
+      <div>
+        <aside>sidebar</aside>
+        <main>feed</main>
+      </div>
     </div>
   )
 }
