952d39da3617bdadeeb94787ac2eb386d53816d5
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-21 01:14:55 -0300

    feat: create comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,50 @@
+// STYLE
+import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react'
+import '../styles/global.css'
+
+export function Comment() {
+  return (
+    <article className={styles.comment}>
+      <img
+        className='avatar'
+        src='https://github.com/davyd-souza.png'
+        alt='Davyd Souza'
+        loading='lazy'
+      />
+
+      <section className={styles.comment__container}>
+        <div className={styles.comment__content}>
+          <header className={styles.comment__header}>
+            <div className={styles.comment__info}>
+              <p className='user__name' data-size='sm'>
+                Davyd Souza
+              </p>
+              <time
+                className='date'
+                data-size='sm'
+                title='19th March at 05:06 PM'
+                dateTime='2023-19-03 17:06:10'
+              >
+                Published 30min ago
+              </time>
+            </div>
+
+            <button className='button btn-icon' data-color='danger'>
+              <Trash />
+            </button>
+          </header>
+
+          <p>Muito bom Camilly, parabéns!! 👏👏</p>
+        </div>
+
+        <footer className={styles.comment__footer}>
+          <button className='button btn-icon' data-color='primary'>
+            <ThumbsUp />
+            20
+          </button>
+        </footer>
+      </section>
+    </article>
+  )
+}
