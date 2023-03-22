dd9922b5589e3724330830df08d95d118dd1d642
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-22 00:34:02 -0300

    feat: implement properties on post

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,77 @@ import { Post } from './components/Post'
 // STYLE
 import styles from './App.module.css'
 
+const POSTS = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Oi-dante.png',
+      name: 'Gustavo Barboza',
+      role: 'Front-end Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Finalmente finalizei meu novo site. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪',
+      },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback 👇' },
+      {
+        type: 'link',
+        content: 'Bytebank-Alura',
+        to: 'https://github.com/Oi-dante/Bytebank-Alura',
+      },
+    ],
+    publishedAt: new Date('2023-03-20 19:02:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/marcelodv.png',
+      name: 'Marcelo Santos',
+      role: 'Software Developer @IBM',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'React é defintivamente a melhor tech que estou usando agora.',
+      },
+      {
+        type: 'paragraph',
+        content:
+          'Antigamente utilizava Vue, porém depois de alguns dias usando React estou simplesmente apaixonado 🥰',
+      },
+    ],
+    publishedAt: new Date('2023-03-19 03:24:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl:
+        'https://media.licdn.com/dms/image/C4D03AQHLtygtGAmNvg/profile-displayphoto-shrink_200_200/0/1661425529298?e=1684368000&v=beta&t=WulqlIOIU-qikqRYGtWJ-KfDbVk3rIFunOdwopRYCco',
+      name: 'Camilly Silva',
+      role: 'Intern @AB InBev',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content:
+          'Ter paciência, persistência, e acreditar no seu próprio potencial não é tão fácil, mas de degrau em degrau acabamos conquistando nossos objetivos.',
+      },
+      {
+        type: 'paragraph',
+        content: 'Fazer parte da Fatec Campinas é um grande orgulho! 💙',
+      },
+      {
+        type: 'link',
+        content: '#fatec',
+      },
+    ],
+    publishedAt: new Date('2022-01-24 13:45:14'),
+  },
+]
+
 export function App() {
   return (
     <div className={styles.App}>
@@ -17,7 +88,14 @@ export function App() {
       <div className={styles.container}>
         <Sidebar />
         <main>
-          <Post />
+          {POSTS.map(({ id, author, content, publishedAt }) => (
+            <Post
+              key={id}
+              author={author}
+              content={content}
+              publishedAt={publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,57 +1,68 @@
+// DEPENDENCY
+import dayjs from 'dayjs'
+
 // COMPONENT
 import { Avatar } from './Avatar'
 
 // STYLE
 import styles from './Post.module.css'
 import { PaperPlaneRight } from 'phosphor-react'
+import { Comment } from './Comment'
 
-export function Post() {
+// TYPE
+interface PostProps {
+  author: {
+    avatarUrl: string
+    name: string
+    role: string
+  }
+  content: { type: string; content: string; to?: string }[]
+  publishedAt: Date
+}
+
+export function Post({ author, content, publishedAt }: PostProps) {
+  const publishedDateFormated = dayjs(publishedAt).format(
+    'MMMM DD[,] YYYY [at] hh[:]mmA'
+  )
+
   return (
     <article className={styles.post}>
       <header className={styles.post__header}>
         <div className={styles.post__author}>
-          <Avatar
-            src='https://media.licdn.com/dms/image/C4D03AQHLtygtGAmNvg/profile-displayphoto-shrink_200_200/0/1661425529298?e=1684368000&v=beta&t=WulqlIOIU-qikqRYGtWJ-KfDbVk3rIFunOdwopRYCco'
-            alt='Camilly Silva'
-          />
+          <Avatar src={author.avatarUrl} alt={author.name} />
           <div>
-            <p className='user__name'>Camilly Silva</p>
-            <span className='user__role'>UI/UX Designer</span>
+            <p className='user__name'>{author.name}</p>
+            <span className='user__role'>{author.role}</span>
           </div>
         </div>
 
         <time
           className='date'
-          title='19th March at 04:36 PM'
-          dateTime='2023-19-03 16:36:10'
+          title={publishedDateFormated}
+          dateTime={publishedAt.toISOString()}
         >
-          Published 1h ago
+          Published {dayjs(publishedAt).fromNow()}
         </time>
       </header>
 
       <section className={styles.content}>
-        <p>Fala pessoal 👋</p>
-
-        <p>
-          Finalmente finalizei meu novo site/portfólio. Foi um baita desafio
-          criar todo o design e codar na unha, mas consegui 💪🏻
-        </p>
-
-        <p>
-          Acesse e deixe seu feedback 👉{' '}
-          <a href='#' className={styles.content__link}>
-            devonlane.design
-          </a>
-        </p>
-
-        <p>
-          <a href='#' className={styles.content__link}>
-            #uiux
-          </a>{' '}
-          <a href='#' className={styles.content__link}>
-            #userexperience
-          </a>
-        </p>
+        {content.map((line, index) => {
+          if (line.type === 'paragraph') {
+            return <p key={index}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return (
+              <p key={index}>
+                <a
+                  className={styles.content__link}
+                  href={line.to ?? '#'}
+                  target='_blank'
+                >
+                  {line.content}
+                </a>
+              </p>
+            )
+          }
+        })}
       </section>
 
       <div className='separator' />
